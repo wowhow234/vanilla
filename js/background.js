fe0561b96a6cb76be660789db40ace59c6796259
wowhow234 <wauhauu@gmail.com>
2023-12-12 14:28:19 +0900

    background

--- js/background.js
+++ js/background.js
@@ -1,7 +1,7 @@
-const images = ["lemon.jpg", "smile.png"];
+// // const images = ["lemon.jpg", "smile.png"];
 
-const chosenImage = images[Math.floor(Math.random() * images.length)];
+// // const chosenImage = images[Math.floor(Math.random() * images.length)];
 
-const bgImage = document.createElement("img");
-bgImage.src = `image/${chosenImage}`;
-document.body.appendChild(bgImage);
+// const bgImage = document.createElement("img");
+// bgImage.src = `image/${chosenImage}`;
+// document.body.appendChild(bgImage);
