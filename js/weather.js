5ca1d3597bf2191a2b3e7f5c477b68bc0e2df15a
wowhow234 <wauhauu@gmail.com>
2023-12-15 15:28:08 +0900

    weather edit

--- js/weather.js
+++ js/weather.js
@@ -4,17 +4,26 @@ function GeoOk(position) {
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
 
-  const url = ` https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  const url = ` https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric&lang=kr`;
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
       const weather = document.querySelector("#weather span:first-child");
-      const city = document.querySelector("#weather span:last-child");
+      const weatherIcon = document.createElement("img");
+      const weatherIconCode = data.weather[0].icon;
+      console.log(weatherIconCode);
+      weatherIcon.src = `image/icons/${weatherIconCode}.png`;
+      document.body.appendChild(weatherIcon);
+
       const temp = weather.nextElementSibling;
+      const city = document.querySelector("#weather span:last-child");
+
       weather.innerText = `${data.weather[0].main}`;
 
       temp.innerText = `${data.main.temp} ℃`;
       city.innerText = data.name;
+      // console.log("데이터", data);
+      console.log("아이콘", data.weather[0].icon);
     });
 }
 
