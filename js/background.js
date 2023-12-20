43801a4f9ef33ef54d6cb2cd51497db705cfeedb
wowhow234 <wauhauu@gmail.com>
2023-12-20 14:54:47 +0900

    delete bg

--- js/background.js
+++
@@ -1,7 +0,0 @@
-// // const images = ["lemon.jpg", "smile.png"];
-
-// // const chosenImage = images[Math.floor(Math.random() * images.length)];
-
-// const bgImage = document.createElement("img");
-// bgImage.src = `image/${chosenImage}`;
-// document.body.appendChild(bgImage);
